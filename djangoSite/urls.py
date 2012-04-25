d09a22851faa97bf3e3ad72ecd9bba86f8ca1be7
Bryan Wolfford <bryan@wolfford.com>
2012-04-24 15:12:37 -1000

    completes the django tutorial part 3 found at https://docs.djangoproject.com/en/1.3/intro/tutorial03/

--- djangoSite/urls.py
+++ djangoSite/urls.py
@@ -1,17 +1,13 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'djangoSite.views.home', name='home'),
-    # url(r'^djangoSite/', include('djangoSite.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
